5bcdfae14e67bd72f740c10afa688708bf1889c0
John Howard <john.k.howard@gmail.com>
2025-02-25 15:33:38 -0500

    Initial Commit

---
+++ OneButton/OneButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneButtonApp.swift
+//  OneButton
+//
+//  Created by John Howard on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct OneButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
