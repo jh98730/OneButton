263331e3a0d269c53fad229c56696b785b5eb00a
John Howard <john.k.howard@gmail.com>
2025-02-25 15:51:57 -0500

    FIst run

--- OneButton/ContentView.swift
+++ OneButton/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var message: String = " "
+    @State var imageString = "hand.thumbsup"
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image(systemName: imageString)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+            Spacer()
+            Button("Press Me") {
+                if message == "You are Awesome!" {
+                    message = "You are Great!"
+                    imageString = "hand.thumbsup"
+                } else {
+                    message = "You are Awesome!"
+                    imageString = "sun.max.fill"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
-        .padding()
+        
     }
 }
 
